cc2362532d2851418933577108ab608fadbd630c
Spudisis <alekasisbb@gmail.com>
2022-07-03 01:02:33 +0300

    button on top, general content

--- src/header/auth/auth.jsx
+++ src/header/auth/auth.jsx
@@ -1,8 +1,10 @@
 import s from "./auth.module.css";
 import { NavLink } from "react-router-dom";
 function Auth() {
+  
+
   return (
-    <div className={s.wrapper}>
+    <div className={s.wrap}>
       <div className={s.authorization}>
         <NavLink to="/authorization" className={s.auth_text}>
           <div id="auth" className={s.refText}>
@@ -14,6 +16,9 @@ function Auth() {
             Регистрация
           </div>
         </NavLink>
+      </div>
+      <div className={s.blockArrow} id="arrow">
+        <img src="./img/arrow.png" alt="Наверх" className={s.arrowImg} />
       </div>
     </div>
   );
