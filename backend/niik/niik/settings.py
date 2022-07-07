38b51f86c913ab2b0dd93379acf16cbfc8624d45
Alexander Vasiliev <danchicdan@gmail.com>
2022-07-07 21:19:20 +0300

    залупа реактивная

--- backend/niik/niik/settings.py
+++ backend/niik/niik/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'niik.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': ['C:/Users/Саня/Desktop/niik/PracnicNIIK/public',],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
