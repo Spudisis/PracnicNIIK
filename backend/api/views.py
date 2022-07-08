b343635518280ec1c18bba085d4378c0b9b74701
Alexander Vasiliev <danchicdan@gmail.com>
2022-07-08 21:23:59 +0300

    merge

---
+++ backend/api/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from .models import Project
+from .serializers import ProjectSerializer
+
+# Create your views here.
+@api_view(['GET'])
+def getRoutes(request):
+    return Response('Our API')
+
+@api_view(['GET'])
+def getProject(request):
+    projects = Project.objects.all()
+    seralizer = ProjectSerializer(projects, many=True)
+    return Response(seralizer.data)
