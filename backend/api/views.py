73d631dbe2ff334be645ad4be2d8ef5e33c57dba
Alexander Vasiliev <danchicdan@gmail.com>
2022-07-09 23:15:34 +0300

    dsadas

--- backend/api/views.py
+++ backend/api/views.py
@@ -3,12 +3,11 @@ from rest_framework.response import Response
 from rest_framework.decorators import api_view
 from .models import Project
 from .serializers import ProjectSerializer
-from jsonMas.json import json
 
 # Create your views here.
 @api_view(['GET'])
 def getRoutes(request):
-    return Response()
+    return Response('fds')
 
 @api_view(['GET'])
 def getProject(request):
