b88288c7e1fc08d104e460fbfb67903f54954f78
Alexander Vasiliev <danchicdan@gmail.com>
2022-07-13 13:05:54 +0300

    dd

--- backend/api/serializers.py
+++ backend/api/serializers.py
@@ -1,7 +1,17 @@
 from rest_framework.serializers import ModelSerializer
-from .models import Project
+from .models import Project,User,Employee
 
 class ProjectSerializer(ModelSerializer):
     class Meta:
         model = Project
+        fields ='__all__'
+
+class UserSerializer(ModelSerializer):
+    class Meta:
+        model = User
+        fields ='__all__'
+
+class EmployeeSerializer(ModelSerializer):
+    class Meta:
+        model = Employee
         fields ='__all__'
--- backend/api/views.py
+++ backend/api/views.py
@@ -1,16 +1,23 @@
 from django.shortcuts import render
 from rest_framework.response import Response
 from rest_framework.decorators import api_view
-from .models import Project
-from .serializers import ProjectSerializer
+from .models import Project,Employee,   User
+from .serializers import ProjectSerializer,EmployeeSerializer,UserSerializer
 
 # Create your views here.
-@api_view(['GET'])
-def getRoutes(request):
-    return Response('fds')
 
 @api_view(['GET'])
 def getProject(request):
     projects = Project.objects.all()
     seralizer = ProjectSerializer(projects, many=True)
+    return Response(seralizer.data)
+@api_view(['GET'])
+def getUser(request):
+    users = User.objects.all()
+    seralizer = UserSerializer(users, many=True)
+    return Response(seralizer.data)
+@api_view(['GET'])
+def getEmployee(request):
+    employees = Employee.objects.all()
+    seralizer = EmployeeSerializer(employees, many=True)
     return Response(seralizer.data)
