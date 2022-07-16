2720f42b9e3f2fb32797a1b74724d85befb6815b
Spudisis <alekasisbb@gmail.com>
2022-07-16 17:25:31 +0300

    tables

--- src/main/profile/orders/orders.jsx
+++ src/main/profile/orders/orders.jsx
@@ -1,7 +1,28 @@
 import s from "./orders.module.css";
+import { useEffect, useState } from "react";
 
 function Orders() {
-  return <div className={s.wrapper}>orders</div>;
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    async function fetchData() {
+      let respons = await fetch("http://127.0.0.1:8000/cards/");
+      let data = await respons.json();
+      setData(data);
+    }
+    fetchData();
+  }, []);
+
+  return (
+    <div className={s.wrapper}>
+      {data.map((element) => (
+        <div className={s.projCard}>
+          <img src={element["url"]} alt="img" className={s.imgProjCard} />
+          <div className={s.nameProjCard}>{element["name"]}</div>
+          <div className={s.textProjCard}>{element["discription"]}</div>
+        </div>
+      ))}
+    </div>
+  );
 }
 
 export default Orders;
